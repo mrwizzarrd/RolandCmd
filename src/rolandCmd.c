d13d90f0fd4207d3e33e8516ad2484bb8b1b36f0
mrwizzarrd <rvermiglio865@gmail.com>
2025-10-26 11:38:46 -0400

    FINISHED DOCS, NOW MAKE SURE TO MAINTAIN DOCS

--- src/rolandCmd.c
+++ src/rolandCmd.c
@@ -700,13 +700,15 @@ int numOfCmds = sizeof(commands) / sizeof(commands[0]);
 
 /**
  * 
- * @brief
+ * @brief Checks of the command is a valid command
  * 
- * @details
+ * @details if the command string cmd matches any of the command names in the commands array, then the validCommand boolean integer is set to 1
  * 
- * @param
+ * @param a pointer to an array of characters which is the comand inputted into the console
  * 
- * @return
+ * @return Two Outcomes:
+ * 	Returns 1 if the command is a valid command name
+ * 	Returns 0 if it is not a valid command name
  * 
  * 	**/
 int compareCommand(char *cmd){
@@ -722,15 +724,18 @@ int compareCommand(char *cmd){
 }
 /**
  * 
- * @brief
+ * @brief searches for the command index of a cetrian command object in the commands array 
  * 
- * @details
+ * @details iterates through the array to find the name of the command and at what index it is being stored at 
  * 
- * @param
+ * @param the string cmd
  * 
- * @return
+ * @return two outcomes:
+ * 	an integer that regerrs to the command index if the command was found
+ * 	-1 if no command was found
  * 
  * 	**/
+
 int getCmdIndex(char *cmd){
 	for(int i = 0; i < numOfCmds; i++){
 		if(strcmp(commands[i].name, cmd) == 0){
@@ -739,16 +744,23 @@ int getCmdIndex(char *cmd){
 	}
 	return -1;
 }
+
 /**
  * 
- * @brief
+ * @brief seperates the command from its arguments
  * 
- * @details
+ * @details tokenizes the command by seperating by the space "add 4 5", becomes "add", "4", "5"
+ * 	it then seperates the first string in the tokenizes command array and the rest become the args array (using the pointer parameter args)
+ * 	then the function checks if there is a Command object that matches the name the user typed and sets the value that *command references to that index if it is found, otherwise it sets it to -1
  * 
- * @param
+ * @param takes three parameters
+ * a pointer to the command string
+ * a pointer to the command index integer
+ * a pointer to the array of strings for arguments
  * 
  * 
  * 	**/
+
 void parseCommand(char *command, int *commandIndex, char **args){
 	char *commandParsed[20];
 	char *token = strtok(command, " ");
